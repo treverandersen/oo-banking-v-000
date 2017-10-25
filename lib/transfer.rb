e8c4677c6b370bbd40e5e1ceba35672c5ece73a1
Trever Andersen <trever.r.andersen@gmail.com>
2017-10-25 02:23:46 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
 
   def reverse_transfer
-  
+
     if status == 'complete'
       sender.balance = sender.balance + amount
       receiver.balance = receiver.balance - amount
