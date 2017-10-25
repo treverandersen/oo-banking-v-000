2c87160f94945519f36d9f014d3dc588b83378b5
Trever Andersen <trever.r.andersen@gmail.com>
2017-10-25 02:08:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
         "Transaction rejected. Please check your account balance."
       end
     else
-      
+
       "Transaction rejected. Please check your account balance."
       self.status = "rejected"
     end
