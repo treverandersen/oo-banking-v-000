b61d27bc48e5d39d179e162bde2f8fcf86cd3258
Trever Andersen <trever.r.andersen@gmail.com>
2017-10-25 00:34:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       if sender.balance > amount
         receiver.balance = receiver.balance + amount
         sender.balance = sender.balance - amount
-        self.status = "completed" 
+        self.status = "complete" 
       else
         "Transaction rejected. Please check your account balance."
       end
